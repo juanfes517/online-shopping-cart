b484fa19251db5078915606b0015c7b6f3ecd28a
Juan Felipe Escobar <juan.escobar15@udea.edu.co>
2025-02-21 21:28:35 -0500

    feat: add User class to domain layer

---
+++ ms-shopping-cart/src/main/java/com/microservice/shoppingcart/domain/model/User.java
@@ -0,0 +1,17 @@
+package com.microservice.shoppingcart.domain.model;
+
+import lombok.*;
+
+@Builder
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private Long id;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
