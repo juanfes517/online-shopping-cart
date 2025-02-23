40845e5cc60ed4a029034a171c0f6a1f3b05f172
Juan Felipe Escobar <juan.escobar15@udea.edu.co>
2025-02-22 19:50:57 -0500

    feat: modify some types

--- ms-shopping-cart/src/main/java/com/microservice/shoppingcart/application/port/input/UserServicePort.java
+++ ms-shopping-cart/src/main/java/com/microservice/shoppingcart/application/port/input/UserServicePort.java
@@ -1,15 +1,16 @@
 package com.microservice.shoppingcart.application.port.input;
 
-import com.microservice.shoppingcart.application.dto.UserDTO;
 import com.microservice.shoppingcart.application.dto.request.LoginRequestDTO;
+import com.microservice.shoppingcart.application.dto.request.UserRequestDTO;
 import com.microservice.shoppingcart.application.dto.response.AuthResponseDTO;
+import com.microservice.shoppingcart.application.dto.response.UserResponseDTO;
 import org.springframework.security.core.Authentication;
 
 public interface UserServicePort {
     AuthResponseDTO login(LoginRequestDTO loginRequestDTO);
     Authentication authenticate(String username, String password);
-    UserDTO CreateUser(UserDTO userDTO);
-    UserDTO UpdateUser(UserDTO userDTO);
-    UserDTO DeleteUser(Long id);
-    UserDTO GetUserById(Long id);
+    UserResponseDTO CreateUser(UserRequestDTO userDTO);
+    UserResponseDTO UpdateUser(UserRequestDTO userDTO);
+    UserResponseDTO DeleteUser(Long id);
+    UserResponseDTO GetUserById(Long id);
 }
