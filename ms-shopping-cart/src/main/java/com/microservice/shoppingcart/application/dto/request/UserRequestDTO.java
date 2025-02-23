9897b68e3c842bad45758dc1799d3f250d35fc1d
Juan Felipe Escobar <juan.escobar15@udea.edu.co>
2025-02-22 19:44:53 -0500

    feat: rename DTO for user request

--- ms-shopping-cart/src/main/java/com/microservice/shoppingcart/application/dto/request/UserRequestDTO.java
+++ ms-shopping-cart/src/main/java/com/microservice/shoppingcart/application/dto/request/UserRequestDTO.java
@@ -7,7 +7,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class CreateUserRequestDTO {
+public class UserRequestDTO {
     private String username;
     private String password;
     private String firstName;
