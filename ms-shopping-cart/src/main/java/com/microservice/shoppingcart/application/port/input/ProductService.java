9bc948d2b92b99c953e06a3e3d08a16d6986580e
Juan Felipe Escobar <juan.escobar15@udea.edu.co>
2025-03-01 16:42:40 -0500

    feat: add port for the product service

---
+++ ms-shopping-cart/src/main/java/com/microservice/shoppingcart/application/port/input/ProductService.java
@@ -0,0 +1,11 @@
+package com.microservice.shoppingcart.application.port.input;
+
+import com.microservice.shoppingcart.application.dto.response.ProductResponseDTO;
+import com.microservice.shoppingcart.domain.model.SelectedProduct;
+
+import java.util.List;
+
+public interface ProductService {
+    List<ProductResponseDTO> getAllProducts();
+    SelectedProduct changeAmount(Long productId, Long amount);
+}
