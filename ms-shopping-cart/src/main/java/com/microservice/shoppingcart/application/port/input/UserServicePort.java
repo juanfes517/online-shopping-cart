b003690a0329a4407b6c59ce1ba298e16e028cc0
Juan Felipe Escobar <juan.escobar15@udea.edu.co>
2025-03-01 15:28:24 -0500

    refactor: change return type

--- ms-shopping-cart/src/main/java/com/microservice/shoppingcart/application/port/input/UserServicePort.java
+++ ms-shopping-cart/src/main/java/com/microservice/shoppingcart/application/port/input/UserServicePort.java
@@ -4,13 +4,14 @@ import com.microservice.shoppingcart.application.dto.request.LoginRequestDTO;
 import com.microservice.shoppingcart.application.dto.request.UserRequestDTO;
 import com.microservice.shoppingcart.application.dto.response.AuthResponseDTO;
 import com.microservice.shoppingcart.application.dto.response.UserResponseDTO;
+import com.microservice.shoppingcart.domain.model.User;
 import org.springframework.security.core.Authentication;
 
 public interface UserServicePort {
     AuthResponseDTO login(LoginRequestDTO loginRequestDTO);
     Authentication authenticate(String username, String password);
-    UserResponseDTO CreateUser(UserRequestDTO userDTO);
-    UserResponseDTO UpdateUser(UserRequestDTO userDTO);
-    UserResponseDTO DeleteUser(Long id);
-    UserResponseDTO GetUserById(Long id);
+    User CreateUser(UserRequestDTO userDTO);
+    User UpdateUser(UserRequestDTO userDTO);
+    User DeleteUser(Long id);
+    User GetUserById(Long id);
 }
