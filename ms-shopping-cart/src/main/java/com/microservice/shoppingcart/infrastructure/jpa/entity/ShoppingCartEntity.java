20c8539cc18fa9b0acb933831ffc95e37ddcaeee
Juan Felipe Escobar <juan.escobar15@udea.edu.co>
2025-03-02 16:52:45 -0500

    feat: add shopping cart entity

---
+++ ms-shopping-cart/src/main/java/com/microservice/shoppingcart/infrastructure/jpa/entity/ShoppingCartEntity.java
@@ -0,0 +1,34 @@
+package com.microservice.shoppingcart.infrastructure.jpa.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Entity
+@Table(name = "shopping_carts")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ShoppingCartEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "total_price")
+    private double totalPrice;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+
+    @OneToMany(mappedBy = "shoppingCart", cascade = CascadeType.REMOVE)
+    private Set<SelectedProductEntity> selectedProducts;
+}
