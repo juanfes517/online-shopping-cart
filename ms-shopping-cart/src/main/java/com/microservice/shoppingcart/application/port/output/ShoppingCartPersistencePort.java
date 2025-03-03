2a114eea58514d99be3374a470a44dc3bc14319e
Juan Felipe Escobar <juan.escobar15@udea.edu.co>
2025-03-02 21:56:59 -0500

    refactor: change return type of findById method

--- ms-shopping-cart/src/main/java/com/microservice/shoppingcart/application/port/output/ShoppingCartPersistencePort.java
+++ ms-shopping-cart/src/main/java/com/microservice/shoppingcart/application/port/output/ShoppingCartPersistencePort.java
@@ -3,9 +3,10 @@ package com.microservice.shoppingcart.application.port.output;
 import com.microservice.shoppingcart.domain.model.ShoppingCart;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ShoppingCartPersistencePort {
     List<ShoppingCart> findAllByUserId(Long userId);
-    ShoppingCart findById(Long id);
+    Optional<ShoppingCart> findById(Long id);
     ShoppingCart save(ShoppingCart shoppingCart);
 }
