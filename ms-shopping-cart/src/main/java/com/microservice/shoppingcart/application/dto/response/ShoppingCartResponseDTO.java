31b5b652cf35323e18e7e9a4ffde1d5ee8853015
Juan Felipe Escobar <juan.escobar15@udea.edu.co>
2025-03-05 22:32:23 -0500

    feat: add DTO for shopping cart and selected products responses

---
+++ ms-shopping-cart/src/main/java/com/microservice/shoppingcart/application/dto/response/ShoppingCartResponseDTO.java
@@ -0,0 +1,22 @@
+package com.microservice.shoppingcart.infrastructure.rest.dto.response;
+
+import com.microservice.shoppingcart.domain.model.User;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ShoppingCartResponseDTO {
+    private Long id;
+    private String name;
+    private String description;
+    private LocalDateTime createdAt;
+    private double totalPrice;
+    private User user;
+    private Set<SelectedProductResponseDTO> selectedProducts;
+}
