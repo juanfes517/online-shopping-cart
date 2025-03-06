9fec463cada1e98335fcf3a96225f2146ca94fe9
Juan Felipe Escobar <juan.escobar15@udea.edu.co>
2025-03-05 23:11:36 -0500

    feat: add request DTO for selected product

---
+++ ms-shopping-cart/src/main/java/com/microservice/shoppingcart/application/dto/request/SelectedProductResquestDTO.java
@@ -0,0 +1,15 @@
+package com.microservice.shoppingcart.application.dto.request;
+
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SelectedProductResquestDTO {
+    private Long id;
+    private String name;
+    private double price;
+    private int amount;
+}
