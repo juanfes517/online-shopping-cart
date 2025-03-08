ef702de3e690d9a8dfcd65f695bdabcebf02a5c6
Juan Felipe Escobar <juan.escobar15@udea.edu.co>
2025-03-08 18:42:22 -0500

    feat: add DTO for exception responses

---
+++ ms-shopping-cart/src/main/java/com/microservice/shoppingcart/infrastructure/rest/advice/ControllerAdvice.java
@@ -0,0 +1,7 @@
+package com.microservice.shoppingcart.infrastructure.rest.advice;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerAdvice {
+}
