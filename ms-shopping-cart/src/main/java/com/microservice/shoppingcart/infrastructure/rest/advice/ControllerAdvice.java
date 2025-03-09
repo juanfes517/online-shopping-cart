51ac4386b9357c078420efa615eceea4d57f346e
Juan Felipe Escobar <juan.escobar15@udea.edu.co>
2025-03-08 19:34:02 -0500

    feat: add exception handler for NotFoundException in ControllerAdvice

--- ms-shopping-cart/src/main/java/com/microservice/shoppingcart/infrastructure/rest/advice/ControllerAdvice.java
+++ ms-shopping-cart/src/main/java/com/microservice/shoppingcart/infrastructure/rest/advice/ControllerAdvice.java
@@ -1,7 +1,27 @@
 package com.microservice.shoppingcart.infrastructure.rest.advice;
 
+import com.microservice.shoppingcart.application.dto.response.ExceptionResponseDTO;
+import com.microservice.shoppingcart.application.exception.NotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.LocalDateTime;
+
+@Slf4j
 @RestControllerAdvice
 public class ControllerAdvice {
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ExceptionResponseDTO handleNotFoundException(NotFoundException e) {
+        log.error(e.getMessage());
+        return ExceptionResponseDTO.builder()
+                .code(String.valueOf(HttpStatus.NOT_FOUND))
+                .message(e.getMessage())
+                .timestamp(LocalDateTime.now())
+                .build();
+    }
 }
