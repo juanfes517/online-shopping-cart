3d4fecbeaa303c6e29f1ad9bd3187b22cb3262e9
Juan Felipe Escobar <juan.escobar15@udea.edu.co>
2025-03-09 17:14:49 -0500

    refactor: change return type of the findByProductCode method

--- ms-inventory/src/main/java/com/microservice/inventory/infrastructure/jpa/repository/ProductRepository.java
+++ ms-inventory/src/main/java/com/microservice/inventory/infrastructure/jpa/repository/ProductRepository.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
-    Optional<Product> findByProductCode(String productCode);
+    Optional<ProductEntity> findByProductCode(String productCode);
 }
