5b6ff69afc126268ba7b63e96223a48bc2caee10
Juan Felipe Escobar <juan.escobar15@udea.edu.co>
2025-03-09 15:23:26 -0500

    refactor: update of product model methods

--- ms-inventory/src/main/java/com/microservice/inventory/domain/model/Product.java
+++ ms-inventory/src/main/java/com/microservice/inventory/domain/model/Product.java
@@ -9,20 +9,21 @@ import lombok.*;
 @AllArgsConstructor
 public class Product {
     private Long id;
+    private String productCode;
     private String name;
     private String description;
     private double rating;
     private double price;
     private int availableQuantity;
 
-    public boolean hasSufficientStock(int quantity) {
-        return this.availableQuantity >= quantity;
+    public boolean canPurchase(int quantity) {
+        if (availableQuantity < quantity) {
+            throw new IndexOutOfBoundsException("Insufficient stock for " + this.name);
+        }
+        return true;
     }
 
     public int substractStock(int quantity) {
-        if (!hasSufficientStock(quantity)) {
-            throw new IndexOutOfBoundsException("Insufficient stock for " + this.name);
-        }
         this.availableQuantity -= quantity;
         return this.availableQuantity;
     }
